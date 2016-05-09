4522f5400b3ff6a7e15cd08a59e1e08b0bc2c90b
Zaneh Williams <zanehmw@gmail.com>
2016-05-09 15:30:49 -0400

    updated graph, ready for data

--- js/graph.controller.js
+++ js/graph.controller.js
@@ -10,47 +10,86 @@
   function GraphControllerFunction($http){
     var graphVm = this;
     $('.container').highcharts({
-      xAxis: {
-        title: {
-               text: 'Mileage'
-           },
-        min: 0,
-        max: 200000
-      },
-      yAxis: {
-        title: {
-               text: 'Price'
-           },
-        min: 0,
-        max: 200000
-      },
-      title: {
-        text: 'Results'
-      },
-      series: [{
-        type: 'line',
-        name: 'Average Market Value',
-        data: [[0, 1.11], [200000, 200000]], //line of best fit function goes here
-        marker: {
-          enabled: false
+        chart: {
+            type: 'scatter',
+            zoomType: 'xy'
         },
-        states: {
-          hover: {
-            lineWidth: 0
-          }
+        title: {
+            text: 'Results'
         },
-        enableMouseTracking: false
-      }, {
-        type: 'scatter',
-        name: 'Cars',
-        data: [1000, 3000, 30000, 180000, 145, 4.2], //
-        marker: {
-          radius: 4
-        }
-      }]
-    });
-  }
+        subtitle: {
+            text: 'Source: None'
+        },
+        xAxis: {
+            title: {
+                enabled: true,
+                text: 'Mileage'
+            },
+            startOnTick: true,
+            endOnTick: true,
+            showLastLabel: true,
+            min: 0,
+            max: 200000
+        },
+        yAxis: {
+            title: {
+                text: 'Price (USD)'
+            },
+            min: 0,
+            max: 200000
+        },
+        // legend: {
+        //     layout: 'vertical',
+        //     align: 'left',
+        //     verticalAlign: 'top',
+        //     x: 100,
+        //     y: 70,
+        //     floating: true,
+        //     backgroundColor: '#FFFFFF',
+        //     borderWidth: 1
+        // },
+        plotOptions: {
+            scatter: {
+                marker: {
+                    radius: 5,
+                    states: {
+                        hover: {
+                            enabled: true,
+                            lineColor: 'rgb(100,100,100)'
+                        }
+                    }
+                },
+                states: {
+                    hover: {
+                        marker: {
+                            enabled: false
+                        }
+                    }
+                },
+                tooltip: {
+                    headerFormat: '<b>{series.name}</b><br>',
+                    pointFormat: 'Mileage: {point.x}, Price: {point.y}'
+                }
+            }
+        },
+        series: [{
+            regression: true ,
+            regressionSettings: {
+           			name: 'Average Market Value',
+                type: 'linear',
+                color:  'rgba(223, 83, 83, .9)'
+            },
+            name: 'Car',
+            color: 'rgba(223, 83, 83, .5)',
+            data: [[1610, 51000], [16000, 59000], [15900, 49000], [157000, 63000], [155000, 200000],
+                [170000, 59000], [159000, 47000], [166000, 69000], [176000, 66000], [100000, 75000],
+                [172000, 55000], [170000, 54000], [172000, 20000], [153000, 42000], [160000, 50000],
+                [147000, 49000], [168000, 49000], [175000, 73000], [157000, 17000], [167000, 68000],
+                [176000, 71000], [164000, 55000], [160000, 48000], [174000, 166000], [163000, 67000]]
 
+        }]
+    })
+      }
 }());
 
 // Load the fonts
---
+++ js/help.controller.js
@@ -0,0 +1,15 @@
+"use strict";
+
+(function(){
+  angular
+  .module("carGraphingApp")
+  .controller("helpController", [
+  "$http",
+  helpControllerFunction]);
+
+  function helpControllerFunction($http){
+    var helpVm = this;
+    console.log("hello")
+  }
+
+})();
