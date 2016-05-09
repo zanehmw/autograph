a9ffb39c6525421f19220289dc4b5cd386eaa534
Zaneh Williams <zanehmw@gmail.com>
2016-05-09 10:35:00 -0400

    got dummy graph to show up on graph.html page

---
+++ js/graph.controller.js
@@ -0,0 +1,47 @@
+"use strict";
+
+(function(){
+  angular
+  .module("carGraphingApp")
+  .controller("graphController", [
+  "$http",
+  GraphControllerFunction])
+
+  function GraphControllerFunction($http){
+    var graphVm = this;
+    $('.container').highcharts({
+      xAxis: {
+        min: -0.5,
+        max: 5.5
+      },
+      yAxis: {
+        min: 0
+      },
+      title: {
+        text: 'Scatter plot with regression line'
+      },
+      series: [{
+        type: 'line',
+        name: 'Regression Line',
+        data: [[0, 1.11], [5, 4.51]],
+        marker: {
+          enabled: false
+        },
+        states: {
+          hover: {
+            lineWidth: 0
+          }
+        },
+        enableMouseTracking: false
+      }, {
+        type: 'scatter',
+        name: 'Observations',
+        data: [1, 1.5, 2.8, 3.5, 3.9, 4.2],
+        marker: {
+          radius: 4
+        }
+      }]
+    });
+  }
+
+}());
