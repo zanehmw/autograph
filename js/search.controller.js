51698689c5908eb1bb18d72b747e24b49c503518
Mary Griffus <mary.e.shultz@gmail.com>
2016-05-10 09:03:34 -0400

    fixed indentations

--- js/search.controller.js
+++ js/search.controller.js
@@ -11,49 +11,40 @@
     var searchVm = this;
 
     this.search = function(){
-     SearchFactory.sendData(this.searchTerms).then(function(res){
-     searchVm.cars = [];
-     searchVm.rawCars = res.data.Item;
+    SearchFactory.sendData(this.searchTerms).then(function(res){
+      searchVm.cars = [];
+      searchVm.rawCars = res.data.Item;
+      console.log("boop bip bip");
+      console.log(res.data.Item);
 
-     console.log("boop bip bip");
-     console.log(res.data.Item);
+      for(var i=0; i<searchVm.rawCars.length; i++){
+         if (searchVm.rawCars[i].ItemSpecifics){
+         var toParse = searchVm.rawCars[i].ItemSpecifics.NameValueList;
+         searchVm.c = {}
+         jQuery.grep(toParse, function(n){
+           if (n.Name == "Make") {searchVm.c.model = n.Value[0]; console.log(n.Value[0]);}
+           if (n.Name == "Model") {searchVm.c.make = n.Value[0]; console.log(n.Value[0]);}
+           if (n.Name == "Year") {searchVm.c.year = n.Value[0]; console.log(n.Value[0]);}
+           if (n.Name == "Mileage") {searchVm.c.mileage = n.Value[0]; console.log(n.Value[0]);}
+         })
 
-     console.log(searchVm.rawCars[0].ItemSpecifics.NameValueList)
-     console.log(searchVm.rawCars[0].ItemSpecifics)
+        console.log(i)
 
-     for(var i=0; i<searchVm.rawCars.length; i++){
-
-       if (searchVm.rawCars[i].ItemSpecifics){
-
-       var toParse = searchVm.rawCars[i].ItemSpecifics.NameValueList;
-       searchVm.c = {}
-       jQuery.grep(toParse, function(n){
-         if (n.Name == "Make") {searchVm.c.model = n.Value[0]; console.log(n.Value[0]);}
-         if (n.Name == "Model") {searchVm.c.make = n.Value[0]; console.log(n.Value[0]);}
-         if (n.Name == "Year") {searchVm.c.year = n.Value[0]; console.log(n.Value[0]);}
-         if (n.Name == "Mileage") {searchVm.c.mileage = n.Value[0]; console.log(n.Value[0]);}
-       })
-
-      console.log(i)
-
-       searchVm.cars.push({
-         make: searchVm.c.make,
-         model: searchVm.c.model,
-         price: searchVm.rawCars[i].ConvertedCurrentPrice.Value,
-         year: searchVm.c.year,
-         mileage: searchVm.c.mileage,
-         location: searchVm.rawCars[i].Location,
-         listing_url: searchVm.rawCars[i].ViewItemURLForNaturalSearch,
-         picture_url: searchVm.rawCars[i].GalleryURL,
-         condition: searchVm.rawCars[i].ConditionDisplayName
-       })
-     }
-   }
-
-     console.log(searchVm.cars[0]);
-
-     })
-
+         searchVm.cars.push({
+           make: searchVm.c.make,
+           model: searchVm.c.model,
+           price: searchVm.rawCars[i].ConvertedCurrentPrice.Value,
+           year: searchVm.c.year,
+           mileage: searchVm.c.mileage,
+           location: searchVm.rawCars[i].Location,
+           listing_url: searchVm.rawCars[i].ViewItemURLForNaturalSearch,
+           picture_url: searchVm.rawCars[i].GalleryURL,
+           condition: searchVm.rawCars[i].ConditionDisplayName
+         })
+        }
+      }
+    console.log(searchVm.cars[0]);
+  })
     }
   }
 
