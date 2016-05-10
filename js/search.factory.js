4f6dfffab5b188d593273f290a87c95a9631796b
Mary Griffus <mary.e.shultz@gmail.com>
2016-05-10 11:26:17 -0400

    put graphing in search controller

--- js/search.factory.js
+++ js/search.factory.js
@@ -11,7 +11,6 @@
     return {
       sendData: function(data){
 
-
         console.log(data);
 
          url = "http://svcs.ebay.com/services/search/FindingService/v1";
@@ -33,68 +32,62 @@
          console.log(url);
 
          return $http.jsonp(url).then(function(res){
-                   console.log("hi111");
-                   console.log(res);
-                   var carInfo = res["data"];
-                   var cars = carInfo.findItemsByKeywordsResponse[0].searchResult[0].item || [];
-                   var urlList = '&itemID=';
-                   var loopCounter = 4;
-                   var resultsArray = [];
-                   var i = 0;
+             console.log("hi111");
+             console.log(res);
+             var carInfo = res["data"];
+             var cars = carInfo.findItemsByKeywordsResponse[0].searchResult[0].item || [];
+             var urlList = '&itemID=';
+             var loopCounter = 4;
+             var resultsArray = [];
+             var i = 0;
 
-                   while(i<99) {
+             while(i<99) {
 
-                    console.log("initial I");
-                    console.log(i);
+              console.log("initial I");
+              console.log(i);
 
-                    console.log(cars.length-(loopCounter*20));
-                     urlList = '&itemID=';
+              console.log(cars.length-(loopCounter*20));
+               urlList = '&itemID=';
 
-                     for(i; i < cars.length-(loopCounter*20); i++){
-                       urlList += cars[i].itemId[0] + ',';
-                     }
+               for(i; i < cars.length-(loopCounter*20); i++){
+                 urlList += cars[i].itemId[0] + ',';
+               }
 
-                     loopCounter=loopCounter-1;
+               loopCounter=loopCounter-1;
 
-                     console.log("what is 'i'");
-                     console.log(i);
-                     console.log(urlList);
-
-
-                     var newUrl = "http://open.api.ebay.com/shopping?";
-                     newUrl += "callname=GetMultipleItems";
-                     newUrl += "&version=963";
-                     newUrl += "&appid=MaryGrif-WDICarPr-PRD-42f839347-07238b74";
-                     newUrl += "&GLOBAL-ID=EBAY-US";
-                     newUrl += "&responseencoding=JSON";
-                     newUrl += "&callbackname=JSON_CALLBACK";
-                     newUrl += "&IncludeSelector=ItemSpecifics";
-                     newUrl += "&REST-PAYLOAD";
-                     newUrl += urlList;
-
-                     console.log(newUrl);
-
-                       $http.jsonp(newUrl).then(function(res){
-                         resultsArray.push(res.data);
-
-                         console.log("BYE111")
-                         console.log(resultsArray.length)
-                         console.log(resultsArray)
-
-                         if (resultsArray.length==5) {
-                            console.log("return")
-                           console.log(resultsArray)
-                           return resultsArray;
-                         }
+               console.log("what is 'i'");
+               console.log(i);
+               console.log(urlList);
 
 
-                       })
+               var newUrl = "http://open.api.ebay.com/shopping?";
+               newUrl += "callname=GetMultipleItems";
+               newUrl += "&version=963";
+               newUrl += "&appid=MaryGrif-WDICarPr-PRD-42f839347-07238b74";
+               newUrl += "&GLOBAL-ID=EBAY-US";
+               newUrl += "&responseencoding=JSON";
+               newUrl += "&callbackname=JSON_CALLBACK";
+               newUrl += "&IncludeSelector=ItemSpecifics";
+               newUrl += "&REST-PAYLOAD";
+               newUrl += urlList;
 
+               console.log(newUrl);
 
+                 $http.jsonp(newUrl).then(function(res){
+                   resultsArray.push(res.data);
 
-                  }
+                   console.log("BYE111")
+                   console.log(resultsArray.length)
+                   console.log(resultsArray)
 
-               });
+                   if (resultsArray.length==5) {
+                      console.log("return")
+                     console.log(resultsArray)
+                     return resultsArray;
+                   }
+                 })
+            }
+         });
        }
      };
    }
