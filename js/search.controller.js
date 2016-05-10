5328c0fc3d54e24e96c4f21c0b6f73735f9ef875
Mary Griffus <mary.e.shultz@gmail.com>
2016-05-10 11:53:16 -0400

    fixed indentation

--- js/search.controller.js
+++ js/search.controller.js
@@ -10,10 +10,15 @@
   function SearchControllerFunction(SearchFactory){
     var searchVm = this;
 
-    searchVm.cars = [{x:100, y:200}, {x:230, y:3043}]
+    searchVm.cars = [{x:2000, y:1000}, {x:130143, y:2330}]
+    searchVm.maxPrice = 2330;
+    searchVm.maxMileage = 130143;
+    
     this.search = function(){
     SearchFactory.sendData(this.searchTerms).then(function(res){
       searchVm.cars = [];
+      searchVm.maxMileage = 0;
+      searchVm.maxPrice = 0;
       searchVm.rawCars = res.data.Item;
       console.log("boop bip bip");
       console.log(res.data.Item);
@@ -24,19 +29,26 @@
          searchVm.c = {}
          jQuery.grep(toParse, function(n){
            if (n.Name == "Make") {searchVm.c.model = n.Value[0]; console.log(n.Value[0]);}
-           if (n.Name == "Model") {searchVm.c.make = n.Value[0]; console.log(n.Value[0]);}
-           if (n.Name == "Year") {searchVm.c.year = n.Value[0]; console.log(n.Value[0]);}
-           if (n.Name == "Mileage") {searchVm.c.mileage = n.Value[0]; console.log(n.Value[0]);}
+           else if (n.Name == "Model") {searchVm.c.make = n.Value[0]; console.log(n.Value[0]);}
+           else if (n.Name == "Year") {searchVm.c.year = n.Value[0]; console.log(n.Value[0]);}
+           else if (n.Name == "Mileage") {searchVm.c.mileage = n.Value[0]; console.log(n.Value[0]);}
          })
 
-        console.log(i)
+         if (searchVm.c.mileage > searchVm.maxMileage) {
+           searchVm.maxMileage = searchVm.c.mileage
+         }
+         if (searchVm.rawCars[i].ConvertedCurrentPrice.Value > searchVm.maxPrice){
+           searchVm.maxPrice = searchVm.rawCars[i].ConvertedCurrentPrice.Value
+         }
+
+         console.log(i)
 
          searchVm.cars.push({
            make: searchVm.c.make,
            model: searchVm.c.model,
-           price: searchVm.rawCars[i].ConvertedCurrentPrice.Value,
+           y: searchVm.rawCars[i].ConvertedCurrentPrice.Value,
            year: searchVm.c.year,
-           mileage: searchVm.c.mileage,
+           x: searchVm.c.mileage,
            location: searchVm.rawCars[i].Location,
            listing_url: searchVm.rawCars[i].ViewItemURLForNaturalSearch,
            picture_url: searchVm.rawCars[i].GalleryURL,
@@ -70,12 +82,12 @@ searchVm.GraphData = function($http){
       endOnTick: true,
       showLastLabel: true,
       min: 0,
-      max: 200000
+      max: (searchVm.maxMileage + 1000)
     },
     yAxis: {
       title: {text: 'Price (USD)'},
       min: 0,
-      max: 200000
+      max: (searchVm.maxPrice + 1000)
     },
 // legend: {
 //     layout: 'vertical',
--- js/search.factory.js
+++ js/search.factory.js
@@ -13,83 +13,81 @@
 
         console.log(data);
 
-         url = "http://svcs.ebay.com/services/search/FindingService/v1";
-         url += "?OPERATION-NAME=findItemsByKeywords";
-         url += "&SERVICE-VERSION=1.0.0";
-         url += "&SECURITY-APPNAME=MaryGrif-WDICarPr-PRD-42f839347-07238b74";
-         url += "&GLOBAL-ID=EBAY-MOTOR";
-         url += "&responseencoding=JSON";
-         url += "&callback=JSON_CALLBACK";
-         url += "&keywords=" + data.carMake + "%20" + data.carModel + "";
-         url += "&buyerPostalCode=" + data.zipCode + "";
-         url += "&itemFilter(0).name=LocalSearchOnly";
-         url += "&itemFilter(0).value=true";
-         url += "&itemFilter(1).name=MaxDistance";
-         url += "&itemFilter(1).value=" + data.radius + "";
-         url += "&paginationInput.entriesPerPage=100";
-         url += "&CategoryID=6001";
+        url = "http://svcs.ebay.com/services/search/FindingService/v1";
+        url += "?OPERATION-NAME=findItemsByKeywords";
+        url += "&SERVICE-VERSION=1.0.0";
+        url += "&SECURITY-APPNAME=MaryGrif-WDICarPr-PRD-42f839347-07238b74";
+        url += "&GLOBAL-ID=EBAY-MOTOR";
+        url += "&responseencoding=JSON";
+        url += "&callback=JSON_CALLBACK";
+        url += "&keywords=" + data.carMake + "%20" + data.carModel + "";
+        url += "&buyerPostalCode=" + data.zipCode + "";
+        url += "&itemFilter(0).name=LocalSearchOnly";
+        url += "&itemFilter(0).value=true";
+        url += "&itemFilter(1).name=MaxDistance";
+        url += "&itemFilter(1).value=" + data.radius + "";
+        url += "&paginationInput.entriesPerPage=100";
+        url += "&CategoryID=6001";
 
-         console.log(url);
+        console.log(url);
 
-         return $http.jsonp(url).then(function(res){
-             console.log("hi111");
-             console.log(res);
-             var carInfo = res["data"];
-             var cars = carInfo.findItemsByKeywordsResponse[0].searchResult[0].item || [];
-             var urlList = '&itemID=';
-             var loopCounter = 4;
-             var resultsArray = [];
-             var i = 0;
+        return $http.jsonp(url).then(function(res){
+          console.log("hi111");
+          console.log(res);
+          var carInfo = res["data"];
+          var cars = carInfo.findItemsByKeywordsResponse[0].searchResult[0].item || [];
+          var urlList = '&itemID=';
+          var loopCounter = 4;
+          var resultsArray = [];
+          var i = 0;
 
-             while(i<99) {
+          while(i<99) {
 
-              console.log("initial I");
-              console.log(i);
+            console.log("initial I");
+            console.log(i);
 
-              console.log(cars.length-(loopCounter*20));
-               urlList = '&itemID=';
+            console.log(cars.length-(loopCounter*20));
+            urlList = '&itemID=';
 
-               for(i; i < cars.length-(loopCounter*20); i++){
-                 urlList += cars[i].itemId[0] + ',';
-               }
-
-               loopCounter=loopCounter-1;
-
-               console.log("what is 'i'");
-               console.log(i);
-               console.log(urlList);
-
-
-               var newUrl = "http://open.api.ebay.com/shopping?";
-               newUrl += "callname=GetMultipleItems";
-               newUrl += "&version=963";
-               newUrl += "&appid=MaryGrif-WDICarPr-PRD-42f839347-07238b74";
-               newUrl += "&GLOBAL-ID=EBAY-US";
-               newUrl += "&responseencoding=JSON";
-               newUrl += "&callbackname=JSON_CALLBACK";
-               newUrl += "&IncludeSelector=ItemSpecifics";
-               newUrl += "&REST-PAYLOAD";
-               newUrl += urlList;
-
-               console.log(newUrl);
-
-                 $http.jsonp(newUrl).then(function(res){
-                   resultsArray.push(res.data);
-
-                   console.log("BYE111")
-                   console.log(resultsArray.length)
-                   console.log(resultsArray)
-
-                   if (resultsArray.length==5) {
-                      console.log("return")
-                     console.log(resultsArray)
-                     return resultsArray;
-                   }
-                 })
+            for(i; i < cars.length-(loopCounter*20); i++){
+              urlList += cars[i].itemId[0] + ',';
             }
-         });
-       }
-     };
-   }
 
+            loopCounter=loopCounter-1;
+
+            console.log("what is 'i'");
+            console.log(i);
+            console.log(urlList);
+
+            var newUrl = "http://open.api.ebay.com/shopping?";
+            newUrl += "callname=GetMultipleItems";
+            newUrl += "&version=963";
+            newUrl += "&appid=MaryGrif-WDICarPr-PRD-42f839347-07238b74";
+            newUrl += "&GLOBAL-ID=EBAY-US";
+            newUrl += "&responseencoding=JSON";
+            newUrl += "&callbackname=JSON_CALLBACK";
+            newUrl += "&IncludeSelector=ItemSpecifics";
+            newUrl += "&REST-PAYLOAD";
+            newUrl += urlList;
+
+            console.log(newUrl);
+
+            $http.jsonp(newUrl).then(function(res){
+              resultsArray.push(res.data);
+
+              console.log("BYE111")
+              console.log(resultsArray.length)
+              console.log(resultsArray)
+
+              if (resultsArray.length==5) {
+                console.log("return")
+                console.log(resultsArray)
+                return resultsArray;
+              }
+            })
+          }
+        });
+      }
+    };
+  }
 })();
