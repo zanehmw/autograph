04620ff1906d9cbadcb6fd17a1f3153dd6bda01e
Mary Griffus <mary.e.shultz@gmail.com>
2016-05-10 12:45:52 -0400

    adds martin's changes to searchcontroller

--- js/search.factory.js
+++ js/search.factory.js
@@ -2,16 +2,17 @@
 "use strict";
 
 (function(){
-  angular
-  .module("carGraphingApp")
-  .factory("SearchFactory", ["$http", SearchFactoryFunction])
+ angular
+ .module("carGraphingApp")
+ .run(function($rootScope){})
+ .factory("SearchFactory", ["$http", SearchFactoryFunction])
 
-  function SearchFactoryFunction($http, $q){
-    var url=[];
-    return {
-      sendData: function(data){
+ function SearchFactoryFunction($http, $q){
+   var url=[];
+   return {
+     sendData: function(data){
 
-        console.log(data);
+       console.log(data);
 
         url = "http://svcs.ebay.com/services/search/FindingService/v1";
         url += "?OPERATION-NAME=findItemsByKeywords";
@@ -32,62 +33,49 @@
         console.log(url);
 
         return $http.jsonp(url).then(function(res){
-          console.log("hi111");
-          console.log(res);
-          var carInfo = res["data"];
-          var cars = carInfo.findItemsByKeywordsResponse[0].searchResult[0].item || [];
-          var urlList = '&itemID=';
-          var loopCounter = 4;
-          var resultsArray = [];
-          var i = 0;
+                  console.log("hi111");
+                  console.log(res);
+                  var carInfo = res["data"];
+                  var cars = carInfo.findItemsByKeywordsResponse[0].searchResult[0].item || [];
+                  var urlList = '&itemID=';
+                  var loopCounter = 4;
+                  var resultsArray = [];
+                  var i = 0;
 
-          while(i<99) {
 
-            console.log("initial I");
-            console.log(i);
+                  while(i<99) {
+                    urlList = '&itemID=';
 
-            console.log(cars.length-(loopCounter*20));
-            urlList = '&itemID=';
+                    for(i; i < cars.length-(loopCounter*20); i++){
+                      urlList += cars[i].itemId[0] + ',';
+                    }
 
-            for(i; i < cars.length-(loopCounter*20); i++){
-              urlList += cars[i].itemId[0] + ',';
-            }
+                    loopCounter=loopCounter-1;
 
-            loopCounter=loopCounter-1;
+                    var newUrl = "http://open.api.ebay.com/shopping?";
+                    newUrl += "callname=GetMultipleItems";
+                    newUrl += "&version=963";
+                    newUrl += "&appid=MaryGrif-WDICarPr-PRD-42f839347-07238b74";
+                    newUrl += "&GLOBAL-ID=EBAY-US";
+                    newUrl += "&responseencoding=JSON";
+                    newUrl += "&callbackname=JSON_CALLBACK";
+                    newUrl += "&IncludeSelector=ItemSpecifics";
+                    newUrl += "&REST-PAYLOAD";
+                    newUrl += urlList;
 
-            console.log("what is 'i'");
-            console.log(i);
-            console.log(urlList);
+                   console.log(newUrl);
 
-            var newUrl = "http://open.api.ebay.com/shopping?";
-            newUrl += "callname=GetMultipleItems";
-            newUrl += "&version=963";
-            newUrl += "&appid=MaryGrif-WDICarPr-PRD-42f839347-07238b74";
-            newUrl += "&GLOBAL-ID=EBAY-US";
-            newUrl += "&responseencoding=JSON";
-            newUrl += "&callbackname=JSON_CALLBACK";
-            newUrl += "&IncludeSelector=ItemSpecifics";
-            newUrl += "&REST-PAYLOAD";
-            newUrl += urlList;
+                   var resultsObj = $http.jsonp(newUrl).then(function(res){
+                     return res.data;
+                   });
+                   resultsArray.push(resultsObj);
+                 }
+                 console.log(resultsArray);
+                 return resultsArray
 
-            console.log(newUrl);
-
-            $http.jsonp(newUrl).then(function(res){
-              resultsArray.push(res.data);
-
-              console.log("BYE111")
-              console.log(resultsArray.length)
-              console.log(resultsArray)
-
-              if (resultsArray.length==5) {
-                console.log("return")
-                console.log(resultsArray)
-                return resultsArray;
-              }
-            })
-          }
-        });
+              });
       }
     };
   }
+
 })();
